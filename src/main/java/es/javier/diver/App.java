083752b58d5573827279424c7be14e87a57a1966
Javier <Javier@192.168.10.47>
2020-01-30 14:05:26 +0100

    bola en pantalla

--- src/main/java/es/javier/diver/App.java
+++ src/main/java/es/javier/diver/App.java
@@ -8,10 +8,10 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import static javafx.scene.input.KeyCode.LEFT;
-import static javafx.scene.input.KeyCode.RIGHT;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -26,8 +26,9 @@ public class App extends Application {
     int posicionbuzo = 50;
     byte buzodireccion = 0;
     short stickHeight = 50;        
-    short stickPosX = (short)((SCENE_HEIGHT-stickHeight)/2);
+    short buzoPosX = (short)((SCENE_HEIGHT-stickHeight)/2);
     byte velocidadbuzo =0 ;
+    byte buzo = 0;
             
     @Override
     public void start(Stage stage) {
@@ -37,6 +38,15 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
         
+        //dibujo del pez con elipse
+        Ellipse ellipse = new Ellipse();
+        ellipse.setCenterX(200);
+        ellipse.setCenterY(400);
+        ellipse.setRadiusX(50);
+        ellipse.setRadiusY(40);
+        ellipse.setFill(Color.ORANGE);
+        root.getChildren().add(ellipse);
+        
         //imagen fondo 1
         Image image2 = new Image(getClass().getResourceAsStream("/images/ocean.gif"));
         ImageView imageView2 = new ImageView(image2);
@@ -45,7 +55,9 @@ public class App extends Application {
         imageView2.setFitHeight(SCENE_HEIGHT);
         imageView2.setFitWidth(SCENE_WIDTH);
         root.getChildren().add(imageView2);
-      
+        
+        
+        
         //imagen fondo 2
         Image image3 = new Image(getClass().getResourceAsStream("/images/ocean2.gif"));
         ImageView imageView3 = new ImageView(image3);
@@ -64,14 +76,18 @@ public class App extends Application {
         imageView1.setFitHeight(80);
         imageView1.setFitWidth(100);  
         
+        
         // CONTROL DEL TECLADO DEL BUZO
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             public void handle(final KeyEvent keyEvent) {
                 switch(keyEvent.getCode()) {
                     case LEFT:
+                      //  buzo.setY(buzo.getY())-150;
                         buzodireccion = -1;
                         break;
+                    
                     case RIGHT:
+                       // buzo.setX(buzo.getX())-150;
                         buzodireccion = 1;
                         break;
                 }                
@@ -96,21 +112,21 @@ public class App extends Application {
                             imageView2.setX(posicionFondo);
                             imageView2.setX(posicionFondo2);
                             
-                            //ANIMACIÓN DEL BUZO
-                            imageView1.setX(stickPosX);
-                            stickPosX += velocidadbuzo * buzodireccion;
-                            if(stickPosX <= 0 || stickPosX >= SCENE_HEIGHT-stickHeight) {
-                                buzodireccion = 0;
-                            }
-                            if(stickPosX <= 0) {
-                                buzodireccion = 0;
-                                stickPosX = 0;
-                            } else if (stickPosX >= SCENE_HEIGHT-stickHeight) {
-                                buzodireccion = 0;
-                                stickPosX = (short)(SCENE_HEIGHT-stickHeight);
-                            }
+                    //ANIMACIÓN DEL BUZO
+                    imageView1.setX(buzoPosX);
+                    buzoPosX += velocidadbuzo * buzodireccion;
+                    if(buzoPosX <= 0 || buzoPosX >= SCENE_HEIGHT-stickHeight) {
+                        buzodireccion = 0;
+                    }
+                    if(buzoPosX <= 0) {
+                        buzodireccion = 0;
+                        buzoPosX = 0;
+                    } else if (buzoPosX >= SCENE_HEIGHT-stickHeight) {
+                        buzodireccion = 0;
+                        buzoPosX = (short)(SCENE_HEIGHT-stickHeight);
+                    }
                             
-                            
+                    
                         }
                     }
                 })
