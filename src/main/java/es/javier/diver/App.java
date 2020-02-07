a54c31d4e9850bad57e122aceb8ff9f7c204c70f
Javier <Javier@192.168.10.47>
2020-02-07 10:33:27 +0100

    he puesto la imagen del tiburon

--- src/main/java/es/javier/diver/App.java
+++ src/main/java/es/javier/diver/App.java
@@ -5,12 +5,14 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -26,7 +28,7 @@ public class App extends Application {
     short buzoHeight = 0;        
     short buzoPosY = (short)((SCENE_HEIGHT-buzoHeight)/2);
     byte velocidadbuzo =0 ;
-    int tiburones = 400;
+    int tiburones = 0;
     int direcciontiburon =1;
     int velocidadtiburon = 0;
             
@@ -37,12 +39,10 @@ public class App extends Application {
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
-        
-        
-        
-        
+
         
         //imagen fondo 1
+        
         Image image2 = new Image(getClass().getResourceAsStream("/images/ocean.gif"));
         ImageView imageOceano1 = new ImageView(image2);
         imageOceano1.setX(posicionFondo); // POSICION HORIZONTALMENTE 
@@ -53,6 +53,7 @@ public class App extends Application {
         
         
         //imagen fondo 2
+        
         Image image3 = new Image(getClass().getResourceAsStream("/images/ocean2.gif"));
         ImageView imageOceano2 = new ImageView(image3);
         imageOceano2.setX(posicionFondo2);  
@@ -61,18 +62,35 @@ public class App extends Application {
         imageOceano2.setFitWidth(SCENE_WIDTH);
         root.getChildren().add(imageOceano2);
         
+        //Rectangulo tiburon
+        Rectangle rectangletiburon = new Rectangle();
+            rectangletiburon.setX(420);
+            rectangletiburon.setY(365);
+            rectangletiburon.setWidth(130);
+            rectangletiburon.setHeight(60);
+        root.getChildren().add(rectangletiburon);      
         
         //imagen tiburon 
+        
         Image image4 = new Image(getClass().getResourceAsStream("/images/tiburon.png"));
         ImageView imagetiburon = new ImageView(image4);
         imagetiburon.setX(420); 
         imagetiburon.setY(365);
-        imagetiburon.setFitHeight(100);
-        imagetiburon.setFitWidth(250);
+        imagetiburon.setFitHeight(60);
+        imagetiburon.setFitWidth(130);
         root.getChildren().add(imagetiburon);
         
+        //rectangulo para el buzo
+        
+        Rectangle rectangle = new Rectangle();
+            rectangle.setX(posicionbuzo);
+            rectangle.setY(posicionbuzo);
+            rectangle.setWidth(100);
+            rectangle.setHeight(80);
+        root.getChildren().add(rectangle);
         
        //imagen buzo
+       
         Image image1 = new Image(getClass().getResourceAsStream("/images/diver.png"));
         ImageView imageBuzo = new ImageView(image1);
         imageBuzo.setX(posicionbuzo); // posicion del buzo horizontal
@@ -81,14 +99,18 @@ public class App extends Application {
         imageBuzo.setFitHeight(80);
         imageBuzo.setFitWidth(100);
         
-            Rectangle r = new Rectangle();
-            r.setX(50);
-            r.setY(50);
-            r.setWidth(200);
-            r.setHeight(100);
-            r.setArcWidth(20);
-            r.setArcHeight(20);
-    
+        //grupo de personaje 
+        Group grupopersonaje = new Group();
+        grupopersonaje.getChildren().add(rectangle);
+        grupopersonaje.getChildren().add(imageBuzo);
+        root.getChildren().add(grupopersonaje);  
+        
+        //grupo de tiburon
+        Group grupotiburon = new Group();
+        grupotiburon.getChildren().add(rectangletiburon);
+        grupotiburon.getChildren().add(imagetiburon);
+        root.getChildren().add(grupotiburon); 
+        
         
         // CONTROL DEL TECLADO DEL BUZO
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
@@ -96,11 +118,11 @@ public class App extends Application {
                 switch(keyEvent.getCode()) {
                     
                     case UP:
-                        if(imageBuzo.getY() > 0){
+                        if(grupopersonaje.getLayoutY() > 0){
                          // buzoPosY = 0;
                         
                             posicionbuzo -=10;
-                            imageBuzo.setY(posicionbuzo);
+                            grupopersonaje.setLayoutY(posicionbuzo);
                         }                             
                         break;
                     
@@ -108,7 +130,7 @@ public class App extends Application {
                         if((imageBuzo.getY()+imageBuzo.getFitHeight()) < 480){
                             // System.out.println(imageBuzo.getY());
                             posicionbuzo  +=10;
-                            imageBuzo.setY(posicionbuzo);
+                            grupopersonaje.setLayoutY(posicionbuzo);
                         }
                         break;
                 }                
@@ -138,15 +160,18 @@ public class App extends Application {
                         }
                     //animacion tiburones 
                      tiburones --  ;
-                     imagetiburon.setX(tiburones);
+                     grupotiburon.setLayoutX(tiburones);
                         velocidadtiburon -=3;
-                     imagetiburon.setX(velocidadtiburon);
+                     grupotiburon.setLayoutX(velocidadtiburon);
                      
                      // colision de buzo y tiburones 
-                     
-                     
-                     
-                     
+                    Shape shapeCollision = Shape.intersect(rectangle, rectangletiburon);
+                    boolean colisionVacia = shapeCollision.getBoundsInLocal().isEmpty();
+                    if(colisionVacia == false && direcciontiburon == 1) {
+                        direcciontiburon = -1;
+                        //score++;
+                       // textScore.setText(String.valueOf(score));
+                    }
                      
                      
                     }
